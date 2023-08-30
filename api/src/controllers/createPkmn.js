0b295bb98c2d78dca53db09496b46b011952428b
Uriel Campos <urielcampos448@gmail.com>
2023-08-30 14:40:05 -0300

    BACKEND FIX

--- api/src/controllers/createPkmn.js
+++ api/src/controllers/createPkmn.js
@@ -6,18 +6,18 @@ async function createPkmn(name, image, hp, attack, defense, speed, height, weigh
 
     const pokemon = await Pokemon.create({
         name,
-        image,
         hp,
         attack,
         defense,
-        speed,
-        height,
-        weight,
     });
+
+    image && (await pokemon.update({ image }));
+    height && (await pokemon.update({ height }));
+    weight && (await pokemon.update({ weight }));
+    speed && (await pokemon.update({ speed }));
 
     await addPokemonTypes(pokemon, types);
 
-    return pokemon.name;
 }
 
 module.exports = createPkmn;
